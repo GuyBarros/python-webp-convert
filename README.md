5f39b63b6afd8d259dfaf15edd8a02e7db35a9c7
Guy Barros <guy@hashicorp.com>
2020-06-23 11:35:54 -0300

    initial working version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # python-webp-convert
 a simple Python script to convert images to webp format for backing up purposes.
+
+
+## Install
+
+if you are running this on a mac, please install webp
+```shell
+brew install webp
+pip install webp-converter
+```
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#this application
+import json
+import os
+import sys
+import pprint
+import argparse
+from PIL import Image
+pp = pprint.PrettyPrinter()
+
+# VAULT_ADDR =  os.environ.get('VAULT_ADDR', 'http://localhost:8200')
+
+def crawlDirectories(inputPath):
+    for dirpath, dirnames, files in os.walk(inputPath):
+        pp.pprint(f'Found directory: {dirpath}')
+        for file_name in files:
+            file, ext = os.path.splitext(dirpath+os.sep+file_name)
+            pp.pprint(ext)
+            if ext in [".png",".jpg",".jpeg"]:
+                convertImage(dirpath+os.sep+file_name)
+            
+def convertImage(infile):
+    file, ext = os.path.splitext(infile)
+    pp.pprint(file)
+    im = Image.open(infile).convert("RGB")
+    im.save(file + ".webp", "WEBP", quality=70 )
+    os.remove(infile)
+
+def main():
+    pp.pprint("Starting")
+    crawlDirectories("/Users/guy/Downloads/Personal/Pictures")
+    
+
+if __name__ == '__main__':
+    main()
